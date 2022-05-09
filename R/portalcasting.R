999692aef1cf2dcb5988d473f932f6617823a027
Prayas Jain <prayas.jn24@gmail.com>
2022-05-09 16:26:47 +0530

    import parallel using roxygen2

--- R/portalcasting.R
+++ R/portalcasting.R
@@ -4,6 +4,7 @@
 #' @importFrom grDevices grey rgb
 #' @importFrom httr content GET stop_for_status
 #' @importFrom jsonlite fromJSON serializeJSON unserializeJSON write_json
+#' @importFrom parallel mclapply
 #' @importFrom portalr download_observations get_future_moons 
 #'  load_trapping_data ndvi summarize_rodent_data weather
 #' @importFrom runjags run.jags runjags.options
